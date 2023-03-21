dd781a49f09b9258a9bb0561444fb744addba3f5
minhluan85 <22125052@student.hcmus.edu.vn>
2023-03-21 09:21:46 +0700

    Commit thu

--- Project/Project/Function.h
+++ Project/Project/Function.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <iostream>
+#include <fstream>
 #include "Year.h"
 #include "Student.h"
 #include "Course.h"
 #include "Semester.h"
 #include "Function.h"
+using namespace std;
--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,1 +1,5 @@
 #include "Function.h"
+int main(){
+    cout << "Hi!";
+    return 0;
+}
