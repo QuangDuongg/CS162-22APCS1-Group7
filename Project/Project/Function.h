ae80dddc213d5f1b1a46424939bfc2095dce4355
minhluan85 <22125052@student.hcmus.edu.vn>
2023-03-22 18:34:06 +0700

    ChangePassword(Full)

--- Project/Project/Function.h
+++ Project/Project/Function.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 #include "Year.h"
 #include "Student.h"
 #include "Course.h"
 #include "Semester.h"
-#include "Function.h"
+#include "Password.h"
+#include "FunctionChangePassword.h"
 using namespace std;
